0d8e12489771436436172cb6bf1d6d916a3ca2c3
kyy <kyyyy8629@gmail.com>
2024-08-12 17:35:38 +0900

    add validator, pipe annotation

--- simple-board/src/board/board.controller.ts
+++ simple-board/src/board/board.controller.ts
@@ -1,7 +1,10 @@
 import { ApiTags } from '@nestjs/swagger';
 import { BoardService } from './board.service';
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, ValidationPipe } from '@nestjs/common';
 import { CreateBoardDto } from './dto/create-board.dto';
+
+// ✅NestJS에서 제공하는 내장 파이프
+// validationPipe, ParseIntPipe, ParseFloatPipe, ParseArrayPipe etc. --------------------------------
 
 // /board로 들어왔을 시
 @Controller('board')
@@ -15,30 +18,33 @@ export class BoardController {
         return this.boardService.findAll();
     }
     // board/13 일시 Param으로 id값 가져오기
+    // 에서 @Param()에 두번째 매개변수로 ParseIntPipe 넣어주면 파이프단에서 형변환이 된다
     @Get(':id')
-    find(@Param('id') id: number) {
+    find(@Param('id', ParseIntPipe ) id: number) {
         console.log(typeof id)
-        return this.boardService.find(Number(id));
+        return this.boardService.find(id);
     }
     // 게시물 생성
     @Post()
+    // ✅new ValidationPipe(): @Body 안에 new ValidationPipe() 객체를 생성해주면 DTO타입에 맞는 유효성 검사가 시작된다
     // dto 생성후 create method의 data type을 dto로 지정해주기
-    create(@Body() data: CreateBoardDto) {
+    create(@Body(new ValidationPipe()) data: CreateBoardDto) {
         return this.boardService.create(data);
     }
+
     // 게시물 업데이트
     @Put(':id')
     update(
-        @Param('id') id: number,
+        @Param('id', ParseIntPipe) id: number,
         @Body() data
     ) {
-        return this.boardService.update(Number(id), data);
+        return this.boardService.update(id, data);
     } 
 
     @Delete(':id')
     remove(
-        @Param('id') id: number,
+        @Param('id', ParseIntPipe) id: number,
     ) {
-        return this.boardService.delete(Number(id));
+        return this.boardService.delete(id);
     }
 }
